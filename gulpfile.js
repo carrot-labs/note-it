c665bfbb5ff479365f81aa9ae539a373a2ddaaf5
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-03-10 11:47:50 -0300

    Update jshint gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp    = require('gulp');
 var jshint  = require('gulp-jshint');
 var plumber = require('gulp-plumber');
 var stylish = require('jshint-stylish');
-var watch   = require('gulp-watch');
 
 gulp.task('jshint', function() {
   gulp.src('server/**/*.js')
     .pipe(plumber())
-    .pipe(watch('server/**/*.js'))
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+
+  gulp.watch(['server/**/*.js'], ['jshint']);
 });
