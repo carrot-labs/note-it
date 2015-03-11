3b76066d6126f1b14c5003b15a3039734e28b46e
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-03-10 22:28:53 -0300

    Create db.js config

---
+++ server/config/db.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://admin:123456@ds045998.mongolab.com:45998/note-it';
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + dbURI);
+});
+
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+mongoose.connection.once('open', function () {
+  console.log('Mongoose default connection is open');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
